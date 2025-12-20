3cd41ab4034576e7ac3b74dd2759b88aee369beb
frederich <frederic.hallein@outlook.com>
2025-12-20 22:56:53 +0100

    put inline keyword back in logger

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,26 @@
 #include "solver.hpp"
 
 int main() {
-    std::vector<std::vector<double>> A = {
-        {2, 1},
-        {5, 7}
-    };
-    std::vector<double> b = {11, 13};
-    std::vector<double> x = {1, 1};
-
-    int K_max = 25;
-    solver::jacobi(A, x, b, K_max, 1e-3);
-
-
     // std::vector<std::vector<double>> A = {
-    //     {16, 3},
-    //     {7, -11}
+    //     {2, 1},
+    //     {5, 7}
     // };
     // std::vector<double> b = {11, 13};
     // std::vector<double> x = {1, 1};
 
-    // int K_max = 7;
-    // solver::gauss_seidel(A, x, b, K_max, 1e-2);
+    // int K_max = 25;
+    // solver::jacobi(A, x, b, K_max, 1e-3);
+
+
+    std::vector<std::vector<double>> A = {
+        {16, 3},
+        {7, -11}
+    };
+    std::vector<double> b = {11, 13};
+    std::vector<double> x = {1, 1};
+
+    int K_max = 7;
+    solver::gauss_seidel(A, x, b, K_max, 1e-2);
 
 
     return 0;
